ad956647dfa95aadb5c9e47b0b7654be411cd254
wildcat <kirillmybox@rambler.ru>
2018-11-06 17:14:54 +0300

    - Реализовал задачу - Подчистил ненужные файлы

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var stocksRefreshInterval = null;
+
+function loadCurrencies() {
+	$('#currency-container').load('/ajax_get_stocks', function() {
+		$('#currency-container').addClass('highlighted');
+		setTimeout(function() {
+			$('#currency-container').removeClass('highlighted');
+		}, 1000);
+	});
+}
+
+$(document).ready(function() {
+	loadCurrencies();
+	
+	stocksRefreshInterval = setInterval(function() {
+		loadCurrencies();
+	}, 15000);
+
+	$('#refresh-stocks').on('click', loadCurrencies);
+});
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,49 @@
 const router = require('express').Router();
 const Promise = require('bluebird');
 const logger = require('log4js').getLogger();
+const fetch = require('isomorphic-fetch');
 
-router.use(function(req, res, next) {
-	logger.info(req.method, req.originalUrl, JSON.stringify(req.body));
+
+router.get('/', function(req, res, next) {
+	Promise.resolve().then(function() {
+
+		res.render('site/index.pug', {});
 	
-	res.render('site/index.pug', {});
+	}).catch(function(err) {
+	
+		logger.error(err.message, err.stack);
+		res.render('errors/500.pug', {});
+	
+	});
+});
+
+
+router.get('/ajax_get_stocks', function(req, res, next) {
+	var stocks = [];
+	
+	fetch('http://phisix-api3.appspot.com/stocks.json').then(function(response) {
+		if (!response.ok) {
+			return Promise.resolve(null);
+		}
+
+		return response.json();
+
+	}).then(function(json) {
+
+		if (json) {
+			stocks = json.stock;
+		}
+
+		res.render('site/stocks.pug', {
+			stocks: stocks
+		});
+	
+	}).catch(function(err) {
+	
+		logger.error(err.message, err.stack);
+		res.render('errors/500.pug', {});
+	
+	});
 });
 
 module.exports = router;
